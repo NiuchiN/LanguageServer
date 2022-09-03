e04db28279fbf0d735d8336fa54f7f3769caa226
NiuchiN <53387464+NiuchiN@users.noreply.github.com>
2022-09-03 15:33:04 +0900

    VisualStudio2022用のクライアントプロジェクトの作成

---
+++ ClientForVS2022/ClientForVS2022Package.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
+
+namespace ClientForVS2022
+{
+
+}
